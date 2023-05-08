c1e2349a63d68239ffc3e2311a77fca8dadb999d
Marco Pertegal <marco.pertegal.prieto@gmail.com>
2023-05-08 12:19:24 +0200

    Resolución errores

--- Concesionario/src/main/java/com/salesianostriana/dam/proyectoFinal/controller/TrabajadorController.java
+++ Concesionario/src/main/java/com/salesianostriana/dam/proyectoFinal/controller/TrabajadorController.java
@@ -1,12 +1,16 @@
 package com.salesianostriana.dam.proyectoFinal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.proyectoFinal.service.TrabajadorService;
 
 @Controller
 public class TrabajadorController {
 	
+	@Autowired
+	@RequestMapping("")
 	private TrabajadorService trabajadorService;
 	
 	public TrabajadorController(TrabajadorService service) {
