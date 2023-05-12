e4f3749ce24e8e8e0f041b2009405b4e95c37bd3
Marco Pertegal <marco.pertegal.prieto@gmail.com>
2023-05-12 14:22:26 +0200

    Entraga parcial

--- concesionario/src/main/java/com/salesianostriana/dam/concesionario/controller/TrabajadorController.java
+++ concesionario/src/main/java/com/salesianostriana/dam/concesionario/controller/TrabajadorController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.concesionario.model.Trabajador;
 import com.salesianostriana.dam.concesionario.service.TrabajadorService;
 
 @Controller
@@ -15,10 +18,23 @@ public class TrabajadorController {
 	@Autowired
 	private TrabajadorService trabajadorService;
 	
-	@GetMapping("/list-trabajadores")
+	@GetMapping("/listTrabajadores")
 	public String index(Model model) {
 		model.addAttribute("listaTrabajadores", trabajadorService.findAll());
-		return "admin/list-trabajadores";
+		return "admin/listTrabajadores";
 	}
 	
+	@GetMapping("/nuevo")
+	public String nuevoTrabajador(Model model) {
+		model.addAttribute("trabajador", new Trabajador());
+		return "admin/formTrabajadores";
+	}
+	
+	@PostMapping("/nuevo/listTrabajadores")
+	public String submitNuevoTrabajador(@ModelAttribute("trabajador") Trabajador trabajador, Model model) {
+
+		trabajadorService.save(trabajador);
+		
+		return "redirect:/admin/listTrabajadores";
+	}
 }
