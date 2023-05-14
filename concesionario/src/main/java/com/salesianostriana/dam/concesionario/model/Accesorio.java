67ab909d8156019c1261834627a483c2b652be09
Marco Pertegal <marco.pertegal.prieto@gmail.com>
2023-05-14 18:05:18 +0200

    Creación de clases modelo

---
+++ concesionario/src/main/java/com/salesianostriana/dam/concesionario/model/Accesorio.java
@@ -0,0 +1,26 @@
+package com.salesianostriana.dam.concesionario.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Accesorio {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String caracteristicas;
+	
+	
+}
---
+++ concesionario/src/main/java/com/salesianostriana/dam/concesionario/model/Motocicleta.java
@@ -0,0 +1,25 @@
+package com.salesianostriana.dam.concesionario.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Motocicleta {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private double peso, cilindrada;
+	
+}
