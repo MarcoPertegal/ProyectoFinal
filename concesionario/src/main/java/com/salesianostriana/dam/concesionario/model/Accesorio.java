d44f87eef78336b47b86a837b4b1666744e888a2
Marco Pertegal <marco.pertegal.prieto@gmail.com>
2023-05-17 13:20:21 +0200

    listado de accesorios

--- concesionario/src/main/java/com/salesianostriana/dam/concesionario/model/Accesorio.java
+++ concesionario/src/main/java/com/salesianostriana/dam/concesionario/model/Accesorio.java
@@ -3,12 +3,24 @@ package com.salesianostriana.dam.concesionario.model;
 import java.util.List;
 
 import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@Data
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Accesorio extends Producto{
 	
 	private String caracteristicas;
